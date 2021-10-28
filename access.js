c6241ee717f1941c11004316e6c58b5785e91bd0
Rob Hess <hessro@oregonstate.edu>
2021-10-28 15:42:54 -0700

    Add code from 10/28 lecture.

--- access.js
+++ access.js
@@ -12,3 +12,41 @@ console.log("== header.parentNode:", header.parentNode)
 
 var photoCardContainer = document.getElementById('photo-card-container')
 console.log("== photoCardContainer:", photoCardContainer)
+
+var photoCards = document.getElementsByClassName('photo-card')
+console.log("== photoCards:", photoCards)
+
+for (var i = 0; i < photoCards.length; i++) {
+  console.log("== photoCards[" + i + "]:", photoCards[i])
+  // if (i === 1) {
+  //   photoCards[i].remove()
+  // }
+}
+
+var links = document.getElementsByTagName('a')
+console.log("== links:", links)
+
+var rightNavitem = document.querySelector('.navitem.right')
+console.log("== rightNavitem:", rightNavitem)
+
+var navitems = document.querySelectorAll('.navitem')
+console.log("== navitems:", navitems)
+
+var firstPhotoCard = photoCards[0]
+console.log("== firstPhotoCard:", firstPhotoCard)
+
+console.log("== firstPhotoCard.textContent:", firstPhotoCard.textContent)
+console.log("== firstPhotoCard.innerHTML:", firstPhotoCard.innerHTML)
+
+console.log("== links[0].href:", links[0].href)
+
+var firstImg = document.querySelector('img')
+console.log("== firstImg.src:", firstImg.src)
+
+// firstPhotoCard.textContent = "Hello!"
+// firstPhotoCard.innerHTML = "<img src='http://placekitten.com/480/480?image=6'/>"
+
+var userSuppliedValue = "Hello, CS 290!"
+var maliciousUserSuppliedValue = "<img src=x onerror='alert(\"You were hacked 🤬\")'/>"
+// firstPhotoCard.innerHTML = "<div class='caption'>" + maliciousUserSuppliedValue + "</div>"
+firstPhotoCard.textContent = maliciousUserSuppliedValue
---
+++ create.js
@@ -0,0 +1,39 @@
+/*
+<section class="photo-card">
+  <div class="img-container">
+    <img class="person-photo-img" src="{{photoUrl}}" />
+  </div>
+  <div class="caption">
+    {{caption}}
+  </div>
+</section>
+*/
+function insertNewPhotoCard(photoUrl, caption) {
+  var photoCardSection = document.createElement('section')
+  photoCardSection.classList.add('photo-card')
+  photoCardSection.classList.add('another-class')
+  photoCardSection.classList.add('yet-another-class')
+  photoCardSection.classList.remove('another-class')
+
+  var imgContainerDiv = document.createElement('div')
+  imgContainerDiv.classList.add('img-container')
+  photoCardSection.appendChild(imgContainerDiv)
+
+  var personPhotoImg = document.createElement('img')
+  personPhotoImg.classList.add('person-photo-img')
+  personPhotoImg.src = photoUrl
+  imgContainerDiv.appendChild(personPhotoImg)
+
+  var captionDiv = document.createElement('div')
+  captionDiv.classList.add('caption')
+  captionDiv.textContent = caption
+  photoCardSection.appendChild(captionDiv)
+
+  console.log("== photoCardSection:", photoCardSection)
+
+  var photoCardContainer = document.getElementById('photo-card-container')
+  photoCardContainer.appendChild(photoCardSection)
+  // document.getElementById('photo-card-container').appendChild(photoCardSection)
+}
+
+insertNewPhotoCard("http://placekitten.com/480/480", "Luke as a kitty")
